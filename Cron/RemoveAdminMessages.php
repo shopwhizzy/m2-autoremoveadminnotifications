da7472c8090342f05ac0545437dc33bc0c1cd4b7
Mgt-Wizards <info@mgt-wizards.com>
2024-02-08 12:18:44 +0000

    initial commit

---
+++ Cron/RemoveAdminMessages.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Copyright © shopwhizzy.com All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace MgtWizards\AutoRemoveAdminNotifications\Cron;
+
+use Magento\AdminNotification\Model\ResourceModel\Inbox\Collection\Unread;
+use Magento\AdminNotification\Model\InboxFactory as InboxModelFactory;
+
+class RemoveAdminMessages
+{
+
+    protected $_messages;
+    protected $inboxModelFactory;
+    protected $logger;
+
+    /**
+     * Constructor
+     *
+     * @param \Psr\Log\LoggerInterface $logger
+     */
+    public function __construct(
+        Unread $messages,
+        InboxModelFactory $inboxModelFactory,
+        \Psr\Log\LoggerInterface $logger
+    ) {
+        $this->_messages = $messages;
+        $this->inboxModelFactory = $inboxModelFactory;
+        $this->logger = $logger;
+    }
+
+    /**
+     * Execute the cron
+     *
+     * @return void
+     */
+    public function execute()
+    {
+        $this->logger->info("Cronjob MgtWizards_RemoveAdminMessages execution started.");
+
+        $items = array_values($this->_messages->getItems());
+        try
+        {
+            foreach ($items as $item)
+            {
+                $model = $this->inboxModelFactory->create()->load($item->getId());
+                if ($model->getId())
+                {
+                    $model->setIsRemove(1)->save();
+                }
+            }
+            $this->logger->info("Cronjob MgtWizards_RemoveAdminMessages: a total of " . count($items) . " record(s) have been removed.");
+        }
+        catch (\Magento\Framework\Exception\LocalizedException $e)
+        {
+            $this->logger->error("Cronjob MgtWizards_RemoveAdminMessages: " . $e->getMessage());
+        }
+        catch (\Exception $e)
+        {
+            $this->logger->error("Cronjob MgtWizards_RemoveAdminMessages: we couldn't remove the messages because of an error.");
+        }
+
+        $this->logger->info("Cronjob MgtWizards_RemoveAdminMessages execution ended.");
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,52 @@
+# Mage2 Module MgtWizards AutoRemoveAdminNotifications
+
+    ``mgtwizards/module-autoremoveadminnotifications``
+
+ - [Main Functionalities](#markdown-header-main-functionalities)
+ - [Installation](#markdown-header-installation)
+ - [Configuration](#markdown-header-configuration)
+ - [Specifications](#markdown-header-specifications)
+ - [Attributes](#markdown-header-attributes)
+
+
+## Main Functionalities
+Magento 2 admin notifications can be annoying. This cron will run every hour and remove all unread messages.
+
+## Installation
+\* = in production please use the `--keep-generated` option
+
+### Type 1: Zip file
+
+ - Unzip the zip file in `app/code/MgtWizards`
+ - Enable the module by running `php bin/magento module:enable MgtWizards_AutoRemoveAdminNotifications`
+ - Apply database updates by running `php bin/magento setup:upgrade`\*
+ - Flush the cache by running `php bin/magento cache:flush`
+
+### Type 2: Composer
+
+ - Make the module available in a composer repository for example:
+    - private repository `repo.magento.com`
+    - public repository `packagist.org`
+    - public github repository as vcs
+ - Add the composer repository to the configuration by running `composer config repositories.repo.magento.com composer https://repo.magento.com/`
+ - Install the module composer by running `composer require mgtwizards/module-autoremoveadminnotifications`
+ - enable the module by running `php bin/magento module:enable MgtWizards_AutoRemoveAdminNotifications`
+ - apply database updates by running `php bin/magento setup:upgrade`\*
+ - Flush the cache by running `php bin/magento cache:flush`
+
+
+## Configuration
+
+
+
+
+## Specifications
+
+ - Cronjob
+	- mgtwizards_autoremoveadminnotifications_removeadminmessages
+
+
+## Attributes
+
+
+
